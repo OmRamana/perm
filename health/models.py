5488a6bf3b1b38a0095250a955781f736d96c855
Om <ramanacomfy@gmail.com>
2022-03-01 14:37:55 -0800

    Whether doctor or client

--- health/models.py
+++ health/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class doctor(models.Model):
     name = models.CharField(max_length=100, blank=True)
+    docstatus = models.BooleanField(default=True)
 
 class client(models.Model):
     name = models.CharField(max_length=100, blank=True)
+    docstatus = models.BooleanField(default=False)
     doctor = models.ForeignKey(doctor,on_delete=models.CASCADE,blank=True, null=True,related_name='doctor')
 
 class appointment(models.Model):
