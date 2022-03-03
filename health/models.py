1fab60a0fcaa75c9096fad42df0fe18e5bdd40fd
Om <ramanacomfy@gmail.com>
2022-03-02 16:21:04 -0800

    model fields update

--- health/models.py
+++ health/models.py
@@ -3,12 +3,10 @@ from django.contrib.auth.models import User
 
 class doctor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, blank=True)
     docstatus = models.BooleanField(default=True)
 
 class client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, blank=True)
     docstatus = models.BooleanField(default=False)
     doctor = models.ForeignKey(doctor,on_delete=models.CASCADE,blank=True, null=True,related_name='doctor')
 
