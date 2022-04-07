f293874ffbe8d726e1862f3df4681847eb0c80f9
Om <ramanacomfy@gmail.com>
2022-04-07 03:11:58 -0700

    initial email settings

--- health/models.py
+++ health/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class doctor(models.Model):
@@ -16,4 +19,9 @@ class appointment(models.Model):
     date = models.DateTimeField()
     doctor = models.ForeignKey(doctor,on_delete=models.CASCADE, related_name='doc')
     client = models.ForeignKey(client,on_delete=models.CASCADE, related_name='client')
+
+    @receiver(post_save)
+    def send_mail_on_create(sender, instance=None, created=False, **kwargs):
+        if created:
+            send_mail('Rest Email','html content','ramanacomfy@gmail.com',["omramana20@gmail.com"],fail_silently=False,)
 
