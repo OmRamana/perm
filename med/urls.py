fee3ab1253fb3ce827de820bce35f0892b657854
Om <ramanacomfy@gmail.com>
2022-04-07 02:52:07 -0700

    login details

--- med/urls.py
+++ med/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('health.urls')),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')), #login details username: o password: o
 ]
